bbec76aa8edf1737ecaa63d1b31255842e02dbb9
Sonali <son@github.ibm.com>
2019-08-09 17:55:28 +0200

    small changes

--- test/io/test_wsi.py
+++ test/io/test_wsi.py
@@ -21,9 +21,9 @@ class CoreTestCase(unittest.TestCase):
         image10x, next_lower_resolution, scale_factor = load(filename)
         image5x, _, _ = load(filename, desired_level=next_lower_resolution)
 
-
         self.assertAlmostEqual(image10x.shape[0], scale_factor*image5x.shape[0])
-        #self.assertAlmostEqual(scale_factor_10x*2 , scale_factor_5x, places=1)
+        #self.assertAlmostEqual(image10x.shape[1], scale_factor*image5x.shape[1])
+
 
     def test_save(self):
         """Test save()."""

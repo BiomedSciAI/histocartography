8f537be85ff206432160880d8ee23923a813602d
Sonali <son@github.ibm.com>
2019-08-12 13:00:46 +0200

    added prediction o wsi given image, model and patch coordinate information

--- histocartography/ml/tumor_slide_prediction.py
+++ histocartography/ml/tumor_slide_prediction.py
@@ -3,20 +3,17 @@ import logging
 import sys
 import numpy as np
 
-'''
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import load_model
 from keras.models import model_from_json
-'''
+
 # setup logging
 
 #logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 log = logging.getLogger('Histocartography::ML::Tumor Slide Prediction')
 h1 = logging.StreamHandler(sys.stdout)
 log.setLevel(logging.INFO)
-#log.setLevel(logging.DEBUG)
-
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 h1.setFormatter(formatter)
 log.addHandler(h1)
@@ -32,27 +29,28 @@ def predict_for_image(patch_info_coordinates, image=None, model_json=None, model
     all_patches = np.array(all_patches)
     log.info('n_patches : {}'.format(all_patches.shape))
 
-    '''
     batch_size = 32
     json_file = open(model_json, 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     model = model_from_json(loaded_model_json)
     model.load_weights(model_weights)
-    print('Model loaded')
-    log.debug('Model loaded')
-
+    log.info('Model loaded')
 
     datagen = ImageDataGenerator(rescale=1. / 255)
     datagen.fit(all_patches)
     n_steps = np.ceil(len(all_patches) / batch_size)
-    y_pred = model.predict_generator(datagen.flow(all_patches, batch_size=batch_size), steps=n_steps)
-    log.debug('len y_pred : {}'.format(len(y_pred)))
-    log.debug('Eg. y_preds : {}'.format(y_pred[0:10]))
-    '''
+    y_pred_prob = model.predict_generator(datagen.flow(all_patches, batch_size=batch_size), steps=n_steps)
+    y_pred = np.argmax(y_pred_prob, axis=1)
 
+    log.info('len y_pred_prob : {}'.format(len(y_pred_prob)))
+    log.info('Eg. y_pred_prob : {}'.format(y_pred_prob[0:10]))
+    y_pred_0 = np.count_nonzero(y_pred==0)
+    y_pred_1 = np.count_nonzero(y_pred==1)
+    n_classes = [y_pred_0, y_pred_1]
+    log.info('n_nroi_roi: {}'.format(n_classes))
 
-    return len(all_patches)
+    return y_pred
 
 
 

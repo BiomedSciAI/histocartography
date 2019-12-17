d5f6eb4b79ca7da5b72f05f5fcd75b0c3831f427
guillaumjaume <guillaume.jaume@epfl.ch>
2019-12-17 11:44:02 +0100

    Implement external argument parser

--- experiments/histo-graph/train_histo_graph.py
+++ experiments/histo-graph/train_histo_graph.py
@@ -4,19 +4,20 @@ Script for training graph-based histocartography models
 """
 import logging
 import sys
-import argparse
 import tempfile
 import importlib
 
 import torch
 import mlflow
 import pytorch_lightning as pl
-
 from brontes import Brontes
+
 from histocartography.utils.io import read_params
 from histocartography.dataloader.pascale_dataloader import make_data_loader
 from histocartography.ml.models.constants import AVAILABLE_MODEL_TYPES, MODEL_TYPE, MODEL_MODULE
 from histocartography.evaluation.evaluator import AccuracyEvaluator, ConfusionMatrixEvaluator
+from histocartography.utils.arg_parser import parse_arguments
+
 
 # setup logging
 log = logging.getLogger('Histocartography::Training')
@@ -31,93 +32,23 @@ log.addHandler(h1)
 # Cuda support
 CUDA = torch.cuda.is_available()
 
-# configure argument parser
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '-d',
-    '--data_path',
-    type=str,
-    help='path to the data.',
-    default='data/',
-    required=False
-)
-parser.add_argument(
-    '-conf',
-    '--config_fpath',
-    type=str,
-    help='path to the config file.',
-    default='',
-    required=False
-)
-parser.add_argument(
-    '-p',
-    '--number_of_workers',
-    type=int,
-    help='number of workers.',
-    default=1,
-    required=False
-)
-parser.add_argument(
-    '-r',
-    '--train_ratio',
-    type=int,
-    help='% of data to use for training.',
-    default=0.8,
-    required=False
-)
-parser.add_argument(
-    '-n',
-    '--model_name',
-    type=str,
-    help='model name.',
-    default='model',
-    required=False
-)
-parser.add_argument(
-    '-b',
-    '--batch_size',
-    type=int,
-    help='batch size.',
-    default=8,
-    required=False
-)
-parser.add_argument(
-    '--epochs', type=int, help='epochs.', default=10, required=False
-)
-parser.add_argument(
-    '-l',
-    '--learning_rate',
-    type=float,
-    help='learning rate.',
-    default=10e-3,
-    required=False
-)
 
-
-def main(arguments):
+def main(args):
     """
     Train SqueezeNet with brontes.
     Args:
-        arguments (Namespace): parsed arguments.
+        args (Namespace): parsed arguments.
     """
-    # create aliases
-    DATA_PATH = arguments.data_path
-    NUMBER_OF_WORKERS = arguments.number_of_workers
-    MODEL_NAME = arguments.model_name
-    BATCH_SIZE = arguments.batch_size
-    EPOCHS = arguments.epochs
-    LEARNING_RATE = arguments.learning_rate
-    TRAIN_RATIO = arguments.train_ratio
 
     # load config file
-    config = read_params(arguments.config_fpath, verbose=True)
+    config = read_params(args.config_fpath, verbose=True)
 
     # make data loaders (train & validation)
     dataloaders = make_data_loader(
-        batch_size=BATCH_SIZE,
-        train_ratio=TRAIN_RATIO,
-        num_workers=NUMBER_OF_WORKERS,
-        path=DATA_PATH,
+        batch_size=args.batch_size,
+        train_ratio=args.train_ratio,
+        num_workers=args.number_of_workers,
+        path=args.data_path,
         config=config,
         cuda=CUDA
     )
@@ -139,7 +70,7 @@ def main(arguments):
     # build optimizer
     optimizer = torch.optim.Adam(
         model.parameters(),
-        lr=LEARNING_RATE
+        lr=args.learning_rate
     )
 
     # define loss function
@@ -147,9 +78,9 @@ def main(arguments):
 
     # mlflow log parameters
     mlflow.log_params({
-        'number_of_workers': NUMBER_OF_WORKERS,
-        'batch_size': BATCH_SIZE,
-        'learning_rate': LEARNING_RATE,
+        'number_of_workers': args.number_of_workers,
+        'batch_size': args.batch_size,
+        'learning_rate': args.learning_rate,
         'config': config
     })
 
@@ -174,17 +105,17 @@ def main(arguments):
 
     # finally, train the model
     if CUDA:
-        trainer = pl.Trainer(gpus=[0], max_nb_epochs=EPOCHS)
+        trainer = pl.Trainer(gpus=[0], max_nb_epochs=args.epochs)
     else:
-        trainer = pl.Trainer(max_nb_epochs=EPOCHS)
+        trainer = pl.Trainer(max_nb_epochs=args.epochs)
     
     trainer.fit(brontes_model)
 
     # save the model to tmp and log it as an mlflow artifact
-    saved_model = f'{tempfile.mkdtemp()}/{MODEL_NAME}.pt'
+    saved_model = f'{tempfile.mkdtemp()}/{args.model_name}.pt'
     torch.save(brontes_model.model, saved_model)
     mlflow.log_artifact(saved_model)
 
 
 if __name__ == "__main__":
-    main(arguments=parser.parse_args())
+    main(args=parse_arguments())
---
+++ histocartography/utils/arg_parser.py
@@ -0,0 +1,66 @@
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-d',
+        '--data_path',
+        type=str,
+        help='path to the data.',
+        default='data/',
+        required=False
+    )
+    parser.add_argument(
+        '-conf',
+        '--config_fpath',
+        type=str,
+        help='path to the config file.',
+        default='',
+        required=False
+    )
+    parser.add_argument(
+        '-p',
+        '--number_of_workers',
+        type=int,
+        help='number of workers.',
+        default=1,
+        required=False
+    )
+    parser.add_argument(
+        '-r',
+        '--train_ratio',
+        type=int,
+        help='% of data to use for training.',
+        default=0.8,
+        required=False
+    )
+    parser.add_argument(
+        '-n',
+        '--model_name',
+        type=str,
+        help='model name.',
+        default='model',
+        required=False
+    )
+    parser.add_argument(
+        '-b',
+        '--batch_size',
+        type=int,
+        help='batch size.',
+        default=8,
+        required=False
+    )
+    parser.add_argument(
+        '--epochs', type=int, help='epochs.', default=10, required=False
+    )
+    parser.add_argument(
+        '-l',
+        '--learning_rate',
+        type=float,
+        help='learning rate.',
+        default=10e-3,
+        required=False
+    )
+
+    return parser.parse_args()

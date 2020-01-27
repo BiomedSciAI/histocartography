c4effd6a40094122700ec9c4f43afcdb01d86592
Lauren Fernandes <frd@zurich.ibm.com>
2020-01-27 16:27:40 +0100

    Changes to WeightedF1 for cluster

--- histocartography/evaluation/evaluator.py
+++ histocartography/evaluation/evaluator.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from sklearn.metrics import confusion_matrix, f1_score
 
 from histocartography.evaluation.base_evaluator import BaseEvaluator
@@ -46,5 +47,6 @@ class WeightedF1(BaseEvaluator):
 
     def __call__(self, logits, labels):
         _, indices = torch.max(logits, dim=1)
-        weighted_f1 = f1_score(labels, indices, average='weighted')
+        weighted_f1 = np.float(f1_score(labels.cpu().numpy(), indices.cpu().numpy(), average='weighted'))
         return torch.FloatTensor([weighted_f1])
+

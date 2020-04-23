84191273f72290dff128fec048dd86f94fa1962a
gja <gja@zurich.ibm.com>
2020-04-23 11:43:34 +0200

    error handling

--- experiments/histo-graph/run_explainer.py
+++ experiments/histo-graph/run_explainer.py
@@ -136,68 +136,73 @@ def main(args):
             label=label
         )
 
-        node_idx = (feats.sum(dim=-1) != 0.).squeeze().cpu()
-        adj = adj[node_idx, :]
-        adj = adj[:, node_idx]
+        try:
 
-        feats = feats[node_idx, :]
-        node_importance = node_importance[node_idx]
+            node_idx = (feats.sum(dim=-1) != 0.).squeeze().cpu()
+            adj = adj[node_idx, :]
+            adj = adj[:, node_idx]
 
-        centroids = data[0].ndata['centroid'].squeeze()
-        centroids = centroids[node_idx, :]
+            feats = feats[node_idx, :]
+            node_importance = node_importance[node_idx]
 
-        explanation = adj_to_networkx(adj, feats, threshold=config['explainer']['adj_thresh'], centroids=centroids)
+            centroids = data[0].ndata['centroid'].squeeze()
+            centroids = centroids[node_idx, :]
 
-        # 1. visualize the original graph
-        show_cg_flag = load_cell_graph(config['model_type'])
-        show_sp_flag = load_superpx_graph(config['model_type'])
-        show_sp_map = False
-        graph_visualizer = GraphVisualization(save_path=args.out_path, show_centroid=True)
-        graph_visualizer(show_cg_flag, show_sp_flag, show_sp_map, data, 1)
+            explanation = adj_to_networkx(adj, feats, threshold=config['explainer']['adj_thresh'], centroids=centroids)
 
-        # 2. visualize the explanation graph
-        graph_visualizer = GraphVisualization(save_path=args.out_path, show_centroid=False)
-        instance_map = data[-1][0]
-        pruned_instance_map = np.zeros(instance_map.shape)
-        for node_id in np.nonzero(node_idx):
-            pruned_instance_map += instance_map * (instance_map == node_id.item() + 1)
-        data = (explanation, data[1], [data[2][0] + '_explanation'], [pruned_instance_map])
-        graph_visualizer(show_cg_flag, show_sp_flag, show_sp_map, data, 1, node_importance=node_importance)
+            # 1. visualize the original graph
+            show_cg_flag = load_cell_graph(config['model_type'])
+            show_sp_flag = load_superpx_graph(config['model_type'])
+            show_sp_map = False
+            graph_visualizer = GraphVisualization(save_path=args.out_path)
+            graph_visualizer(show_cg_flag, show_sp_flag, show_sp_map, data, 1)
 
-        # 3. save meta data in json file
-        meta_data = {}
+            # 2. visualize the explanation graph
+            graph_visualizer = GraphVisualization(save_path=args.out_path)
+            instance_map = data[-1][0]
+            pruned_instance_map = np.zeros(instance_map.shape)
+            for node_id in np.nonzero(node_idx):
+                pruned_instance_map += instance_map * (instance_map == node_id.item() + 1)
+            data = (explanation, data[1], [data[2][0] + '_explanation'], [pruned_instance_map])
+            graph_visualizer(show_cg_flag, show_sp_flag, show_sp_map, data, 1, node_importance=node_importance)
 
-        # 3-a config file
-        meta_data['config'] = config['explainer']  # @TODO set all the params from the config file...
-        meta_data['config']['number_of_epochs'] = args.epochs
-        meta_data['config']['learning_rate'] = args.learning_rate
+            # 3. save meta data in json file
+            meta_data = {}
 
-        meta_data['output'] = {}
+            # 3-a config file
+            meta_data['config'] = config['explainer']  # @TODO set all the params from the config file...
+            meta_data['config']['number_of_epochs'] = args.epochs
+            meta_data['config']['learning_rate'] = args.learning_rate
 
-        # 3-b label
-        meta_data['output']['label_index'] = label.item()
-        meta_data['output']['label_set'] = [val for key, val in label_to_tumor_type.items()]
-        meta_data['output']['label'] = label_to_tumor_type[str(label.item())]
+            meta_data['output'] = {}
 
-        # 3-c original graph properties
-        meta_data['output']['original'] = {}
-        meta_data['output']['original']['logits'] = list(np.around(orig_pred, 2).astype(float))
-        meta_data['output']['original']['number_of_nodes'] = cell_graph.number_of_nodes()
-        meta_data['output']['original']['number_of_edges'] = cell_graph.number_of_edges()
-        meta_data['output']['original']['prediction'] = label_to_tumor_type[str(np.argmax(orig_pred))]
+            # 3-b label
+            meta_data['output']['label_index'] = label.item()
+            meta_data['output']['label_set'] = [val for key, val in label_to_tumor_type.items()]
+            meta_data['output']['label'] = label_to_tumor_type[str(label.item())]
 
-        # 3-d explanation graph properties
-        meta_data['output']['explanation'] = {}
-        meta_data['output']['explanation']['number_of_nodes'] = explanation.number_of_nodes()
-        meta_data['output']['explanation']['number_of_edges'] = explanation.number_of_edges()
-        meta_data['output']['explanation']['logits'] = list(np.around(exp_pred, 2).astype(float))
-        meta_data['output']['explanation']['prediction'] = label_to_tumor_type[str(np.argmax(exp_pred))]
+            # 3-c original graph properties
+            meta_data['output']['original'] = {}
+            meta_data['output']['original']['logits'] = list(np.around(orig_pred, 2).astype(float))
+            meta_data['output']['original']['number_of_nodes'] = cell_graph.number_of_nodes()
+            meta_data['output']['original']['number_of_edges'] = cell_graph.number_of_edges()
+            meta_data['output']['original']['prediction'] = label_to_tumor_type[str(np.argmax(orig_pred))]
 
-        # 3-e write to json
-        write_json(complete_path(args.out_path, data[-2][0] + '.json'), meta_data)
+            # 3-d explanation graph properties
+            meta_data['output']['explanation'] = {}
+            meta_data['output']['explanation']['number_of_nodes'] = explanation.number_of_nodes()
+            meta_data['output']['explanation']['number_of_edges'] = explanation.number_of_edges()
+            meta_data['output']['explanation']['logits'] = list(np.around(exp_pred, 2).astype(float))
+            meta_data['output']['explanation']['prediction'] = label_to_tumor_type[str(np.argmax(exp_pred))]
 
-        # 4. aggregate all the information in a single user-friendly image
-        agg_and_plot_interpretation(meta_data, save_path=args.out_path, image_name=data[-2][0])
+            # 3-e write to json
+            write_json(complete_path(args.out_path, data[-2][0] + '.json'), meta_data)
+
+            # 4. aggregate all the information in a single user-friendly image
+            agg_and_plot_interpretation(meta_data, save_path=args.out_path, image_name=data[-2][0])
+
+        else:
+            print('An error occured in viz...')
 
 
 if __name__ == "__main__":

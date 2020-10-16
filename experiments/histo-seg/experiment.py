3e5a50e915367bbc73e79b81d1c4fb5037aa0d37
Valentin Anklin <ANV@zurich.ibm.com>
2020-10-16 12:37:44 +0200

    Make saving optional and add subset option

--- experiments/histo-seg/experiment.py
+++ experiments/histo-seg/experiment.py
@@ -6,7 +6,7 @@ import multiprocessing
 import sys
 from functools import partial
 from pathlib import Path
-from typing import Callable, Tuple
+from typing import Callable, Tuple, Union
 
 import pandas as pd
 import yaml
@@ -16,8 +16,12 @@ from eth import DATASET_PATH, IMAGES_DF, PREPROCESS_PATH
 from feature_extraction import FeatureExtractor
 from graph_builders import BaseGraphBuilder
 from superpixel import SuperpixelExtractor
-from utils import (dynamic_import_from, get_next_version_number, read_image,
-                   start_logging)
+from utils import (
+    dynamic_import_from,
+    get_next_version_number,
+    read_image,
+    start_logging,
+)
 
 
 def process_image(
@@ -25,6 +29,7 @@ def process_image(
     superpixel_extractor: Callable[[], SuperpixelExtractor],
     feature_extractor: Callable[[], FeatureExtractor],
     graph_builder: Callable[[], BaseGraphBuilder],
+    save: bool,
 ) -> None:
     """Process an image given the row of the metadata dataframe
 
@@ -50,36 +55,61 @@ def process_image(
     # TODO: stain normalization
 
     # Superpixels
-    superpixels = superpixel_extractor.process_and_save(
-        input_image=image, output_name=name
-    )
+    if save:
+        superpixels = superpixel_extractor.process_and_save(
+            input_image=image, output_name=name
+        )
+    else:
+        superpixels = superpixel_extractor.process(input_image=image)
 
     # Features
-    features = feature_extractor.process_and_save(
-        input_image=image, superpixels=superpixels, output_name=name
-    )
+    if save:
+        features = feature_extractor.process_and_save(
+            input_image=image, superpixels=superpixels, output_name=name
+        )
+    else:
+        features = feature_extractor.process(input_image=image, superpixels=superpixels)
 
     # Graph building
-    graph_builder.process_and_save(
-        structure=superpixels, features=features, output_name=name
-    )
+    if save:
+        graph_builder.process_and_save(
+            structure=superpixels, features=features, output_name=name
+        )
+    else:
+        graph_builder.process(structure=superpixels, features=features)
 
 
-def preprocessing(config: dict, test: bool = False, cores: int = 1, **kwargs):
+def preprocessing(
+    config: dict,
+    test: bool = False,
+    save: bool = True,
+    cores: int = 1,
+    subsample: Union[None, int] = None,
+    **kwargs,
+):
     """Runs the preprocessing with a given number of cores
 
     Args:
         cores (int): Number of cores to use
     """
     if len(kwargs) > 0:
-        logging.warning(f'Unmatched arguments: {kwargs}')
+        logging.warning(f"Unmatched arguments: {kwargs}")
     images_metadata = pd.read_pickle(IMAGES_DF)
+
+    # Handle test mode
     if test:
-        images_metadata = images_metadata.iloc[[0, 1]]
+        images_metadata = images_metadata.iloc[[0]]
         cores = 1
         config["superpixel_extractor"]["params"]["nr_superpixels"] = 10
 
-    if not PREPROCESS_PATH.exists():
+    # Handle subsample mode
+    if subsample is not None:
+        assert (
+            1 <= subsample <= len(images_metadata)
+        ), f"Subsample needs to be larger than 1 and smaller than the number of rows but is {subsample}"
+        images_metadata = images_metadata.sample(subsample)
+
+    if save and not PREPROCESS_PATH.exists():
         PREPROCESS_PATH.mkdir()
 
     # -------------------------------------------------------------------------- SUPERPIXEL EXTRACTION
@@ -90,27 +120,23 @@ def preprocessing(config: dict, test: bool = False, cores: int = 1, **kwargs):
         base_path=PREPROCESS_PATH,
         **superpixel_config.get("params", {}),
     )
-    feature_path = superpixel_extractor().mkdir()
+    feature_path = superpixel_extractor().mkdir() if save else None
 
     # -------------------------------------------------------------------------- FEATURE EXTRACTION
     feature_config = config["feature_extractor"]
     feature_class = dynamic_import_from("feature_extraction", feature_config["class"])
     feature_extractor = partial(
-        feature_class,
-        base_path=feature_path,
-        **feature_config.get("params", {})
+        feature_class, base_path=feature_path, **feature_config.get("params", {})
     )
-    graph_path = feature_extractor().mkdir()
+    graph_path = feature_extractor().mkdir() if save else None
 
     # -------------------------------------------------------------------------- GRAPH CONSTRUCTION
     graph_config = config["graph_builder"]
     graph_class = dynamic_import_from("graph_builders", graph_config["class"])
     graph_builder = partial(
-        graph_class,
-        base_path=graph_path,
-        **graph_config.get("params", {})
+        graph_class, base_path=graph_path, **graph_config.get("params", {})
     )
-    final_path = graph_builder().mkdir()
+    final_path = graph_builder().mkdir() if save else None
     # --------------------------------------------------------------------------
 
     worker_task = partial(
@@ -118,11 +144,14 @@ def preprocessing(config: dict, test: bool = False, cores: int = 1, **kwargs):
         superpixel_extractor=superpixel_extractor,
         feature_extractor=feature_extractor,
         graph_builder=graph_builder,
+        save=save,
     )
 
     if cores == 1:
-        for x in tqdm(images_metadata.iterrows(), total=len(images_metadata)):
-            worker_task(x)
+        for image_metadata in tqdm(
+            images_metadata.iterrows(), total=len(images_metadata)
+        ):
+            worker_task(image_metadata)
     else:
         # Run multiprocessing
         worker_pool = multiprocessing.Pool(cores)
@@ -139,46 +168,51 @@ def preprocessing(config: dict, test: bool = False, cores: int = 1, **kwargs):
         worker_pool.join()
 
     # Create dataset version file
-    next_version = get_next_version_number(DATASET_PATH)
-    dataset_file_path = DATASET_PATH / f"v{next_version}.pickle"
-    dataset_information = list()
-    for file in final_path.iterdir():
-        name = file.name.split(".")[0]
-        assert name in images_metadata.index
-        row = images_metadata.loc[name]
-        dataset_information.append(
-            (
-                name,
-                row.path,
-                file,
-                feature_path.name,
-                graph_path.name,
-                final_path.name,
-                next_version,
+    if save:
+        next_version = get_next_version_number(DATASET_PATH)
+        dataset_file_path = DATASET_PATH / f"v{next_version}.pickle"
+        dataset_information = list()
+        for file in final_path.iterdir():
+            name = file.name.split(".")[0]
+            assert name in images_metadata.index
+            row = images_metadata.loc[name]
+            dataset_information.append(
+                (
+                    name,
+                    row.path,
+                    file,
+                    feature_path.name,
+                    graph_path.name,
+                    final_path.name,
+                    next_version,
+                )
             )
+        dataset_dataframe = pd.DataFrame(
+            dataset_information,
+            columns=[
+                "name",
+                "image_path",
+                "graph_path",
+                "superpixel_extractor",
+                "feature_extractor",
+                "graph_builder",
+                "version",
+            ],
         )
-    dataset_dataframe = pd.DataFrame(
-        dataset_information,
-        columns=[
-            "name",
-            "image_path",
-            "graph_path",
-            "superpixel_extractor",
-            "feature_extractor",
-            "graph_builder",
-            "version",
-        ],
-    )
-    dataset_dataframe = dataset_dataframe.set_index("name")
-    dataset_dataframe.to_pickle(dataset_file_path)
+        dataset_dataframe = dataset_dataframe.set_index("name")
+        dataset_dataframe.to_pickle(dataset_file_path)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("command", choices=["preprocess", "train"])
     parser.add_argument("--config", type=str, default="default.yml")
+    parser.add_argument("--subsample", type=int, default=-1)
     parser.add_argument("--test", action="store_const", const=True, default=False)
+    parser.add_argument("--nosave", action="store_const", const=True, default=False)
     args = parser.parse_args()
+
+    print(args)
 
     start_logging()
     assert Path(args.config).exists(), f"Config path does not exist: {args.config}"
@@ -196,7 +230,13 @@ if __name__ == "__main__":
         assert (
             "params" in config
         ), f"params not defined in config {args.config}: {config.keys()}"
-        preprocessing(test=args.test, config=config["stages"], **config["params"])
+        preprocessing(
+            test=args.test,
+            config=config["stages"],
+            save=(not args.nosave),
+            subsample=None if args.subsample < 0 else args.subsample,
+            **config["params"],
+        )
     elif args.command == "train":
         logging.info("Training GNN")
         raise NotImplementedError

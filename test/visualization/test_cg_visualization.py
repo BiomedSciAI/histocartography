bc695ac94a73f60bfb6813f48f83f82397cb3445
Guillaume Jaume <gja@zurich.ibm.com>
2021-03-22 14:27:48 +0100

    use relative path to load images/graphs

--- test/visualization/test_cg_visualization.py
+++ test/visualization/test_cg_visualization.py
@@ -1,37 +1,41 @@
 """Unit test for visualisation.graph_visualization"""
 import unittest
 import numpy as np
-import cv2
-import torch
 import yaml
 import os
-from copy import deepcopy
 from dgl.data.utils import load_graphs
+import shutil
 
 from histocartography.utils.io import load_image, save_image
 from histocartography.interpretability.grad_cam import GraphGradCAMPPExplainer
-from histocartography.visualisation.graph_visualization import (
-    GraphVisualization,
-    OverlayGraphVisualization,
-)
-
-BASE_S3 = "s3://mlflow/"
+from histocartography.visualisation.graph_visualization import OverlayGraphVisualization
 
 
 class GraphVizTestCase(unittest.TestCase):
     """GraphVizTestCase class."""
 
-    def setUp(self):
-        """Setting up the test."""
+    @classmethod
+    def setUpClass(self):
+        self.current_path = os.path.dirname(__file__)
+        self.data_path = os.path.join(self.current_path, '..', 'data')
+        self.image_path = os.path.join(self.data_path, 'images')
+        self.image_name = '283_dcis_4.png'
+        self.graph_path = os.path.join(self.data_path, 'cell_graphs')
+        self.graph_name = '283_dcis_4.bin'
+        self.out_path = os.path.join(self.data_path, 'visualization_test')
+        if os.path.exists(self.out_path) and os.path.isdir(self.out_path):
+            shutil.rmtree(self.out_path) 
+        os.makedirs(self.out_path)
 
     def test_overlay_with_explanation(self):
-        """Test Graph visualization with new class."""
+        """Test Graph visualization."""
+
         # 1. load a cell graph
-        cell_graph, _ = load_graphs("test/data/cell_graphs/283_dcis_4.bin")
+        cell_graph, _ = load_graphs(os.path.join(self.graph_path, self.graph_name))
         cell_graph = cell_graph[0]
 
         # 2. load the corresponding image
-        image = np.array(load_image("test/data/images/283_dcis_4.png"))
+        image = np.array(load_image(os.path.join(self.image_path, self.image_name)))
 
         # 3. run the explainer
         explainer = GraphGradCAMPPExplainer(model_path="test/data/models/cg_3_classes")
@@ -62,27 +66,37 @@ class GraphVizTestCase(unittest.TestCase):
         )
 
         # 5. save output image
-        save_image("tmp/cg_viz_with_explanation.png", out)
+        save_image(
+            os.path.join(
+                self.out_path,
+                self.image_name.replace('.png', '') + '_cg_explanation.png'),
+            out
+        )
 
     def test_overlay_graph_viz(self):
-        """Test Graph visualization with new class."""
+        """Test Graph visualization."""
+
         # 1. load a cell graph
-        cell_graph, _ = load_graphs("test/data/cell_graphs/283_dcis_4.bin")
+        cell_graph, _ = load_graphs(os.path.join(self.graph_path, self.graph_name))
         cell_graph = cell_graph[0]
 
         # 2. load the corresponding image
-        image = np.array(load_image("test/data/images/283_dcis_4.png"))
+        image = np.array(load_image(os.path.join(self.image_path, self.image_name)))
 
         # 3. run the visualization
         visualizer = OverlayGraphVisualization()
         out = visualizer.process(image, cell_graph)
 
         # 5. save output image
-        save_image("tmp/cg_viz_overlay.png", out)
-
+        save_image(
+            os.path.join(
+                self.out_path,
+                self.image_name.replace('.png', '') + '_cg_overlay.png'),
+            out
+        )
     def tearDown(self):
         """Tear down the tests."""
 
 
-# if __name__ == "__main__":
-#     unittest.main()
+if __name__ == "__main__":
+    unittest.main()

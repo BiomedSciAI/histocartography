aa4c33879bd69c606e9dc6829f98ad8f422c94a5
Guillaume Jaume <gja@zurich.ibm.com>
2021-04-01 16:55:01 +0200

    gradcam ut

--- test/interpretability/test_graphgradcam.py
+++ test/interpretability/test_graphgradcam.py
@@ -11,10 +11,10 @@ import shutil
 from dgl.data.utils import load_graphs
 
 from histocartography.interpretability import GraphGradCAMExplainer, GraphGradCAMPPExplainer
+from histocartography.ml import CellGraphModel
 from histocartography.utils.graph import set_graph_on_cuda
 from histocartography.utils.io import download_test_data
 
-BASE_S3 = 's3://mlflow/'
 IS_CUDA = torch.cuda.is_available()
 
 
@@ -33,7 +33,6 @@ class GraphGradCAMTestCase(unittest.TestCase):
             shutil.rmtree(self.out_path) 
         os.makedirs(self.out_path)
 
-    @unittest.skip("In dev.")
     def test_graphgradcam(self):
         """
         Test Graph GradCAM.
@@ -42,16 +41,24 @@ class GraphGradCAMTestCase(unittest.TestCase):
         # 1. load a graph
         graph, _ = load_graphs(os.path.join(self.graph_path, self.graph_name))
         graph = graph[0]
-        graph.ndata['feat'] = torch.cat(
-            (graph.ndata['feat'].float(),
-            (graph.ndata['centroid']).float()),
-            dim=1
-        )
         graph = set_graph_on_cuda(graph) if IS_CUDA else graph
+        node_dim = graph.ndata['feat'].shape[1]
 
-        # 2. run the explainer
+        # 2. create model 
+        config_fname = os.path.join(self.current_path, 'config', 'cg_bracs_cggnn_3_classes_gin.yml')
+        with open(config_fname, 'r') as file:
+            config = yaml.load(file)
+
+        model = CellGraphModel(
+            gnn_params=config['gnn_params'],
+            classification_params=config['classification_params'],
+            node_dim=node_dim,
+            num_classes=3
+        )
+
+        # 3. run the explainer
         explainer = GraphGradCAMExplainer(
-            model_path='https://ibm.box.com/shared/static/aoogy0516lsp9vaxgw1tr9mdu5nycvvb.pt'
+            model=model
         )
         importance_scores, logits = explainer.process(graph)
 
@@ -60,7 +67,6 @@ class GraphGradCAMTestCase(unittest.TestCase):
         self.assertIsInstance(logits, np.ndarray)
         self.assertEqual(graph.number_of_nodes(), importance_scores.shape[0])
 
-    @unittest.skip("In dev.")
     def test_graphgradcampp(self):
         """
         Test Graph GradCAM++.
@@ -69,16 +75,24 @@ class GraphGradCAMTestCase(unittest.TestCase):
         # 1. load a graph
         graph, _ = load_graphs(os.path.join(self.graph_path, self.graph_name))
         graph = graph[0]
-        graph.ndata['feat'] = torch.cat(
-            (graph.ndata['feat'].float(),
-            (graph.ndata['centroid']).float()),
-            dim=1
-        )
         graph = set_graph_on_cuda(graph) if IS_CUDA else graph
+        node_dim = graph.ndata['feat'].shape[1]
+
+        # 2. create model 
+        config_fname = os.path.join(self.current_path, 'config', 'cg_bracs_cggnn_3_classes_gin.yml')
+        with open(config_fname, 'r') as file:
+            config = yaml.load(file)
+
+        model = CellGraphModel(
+            gnn_params=config['gnn_params'],
+            classification_params=config['classification_params'],
+            node_dim=node_dim,
+            num_classes=3
+        )
 
         # 2. run the explainer
         explainer = GraphGradCAMPPExplainer(
-            model_path='https://ibm.box.com/shared/static/aoogy0516lsp9vaxgw1tr9mdu5nycvvb.pt'
+            model=model
         )
         importance_scores, logits = explainer.process(graph)
 

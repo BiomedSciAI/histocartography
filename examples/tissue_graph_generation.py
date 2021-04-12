f2621fd22a737156e95263847f41f5a5e2d71894
Guillaume Jaume <gja@zurich.ibm.com>
2021-04-12 15:40:13 +0200

    example: building a tissue graph

--- examples/tissue_graph_generation.py
+++ examples/tissue_graph_generation.py
@@ -1,116 +1,88 @@
-"""Build tissue graphs from a list of images"""
-import numpy as np
-import cv2 
-import torch 
-import yaml
+"""
+Example: Extract a Tissue Graph from an H&E image. 
+
+As used in:
+- "Hierarchical Graph Representations in Digital Pathology", Pati et al, 2021. 
+- "HACT-Net: A Hierarchical Cell-to-Tissue Graph Neural Network for Histopathological Image Classification", Pati et al, MCCAI-W, 2020. 
+"""
+
 import os 
-from dgl.data.utils import save_graphs
-import argparse
+from glob import glob 
+from PIL import Image
+import numpy as np
 from tqdm import tqdm  
-import h5py
+from dgl.data.utils import save_graphs
 
-from histocartography.preprocessing import ColorMergedSuperpixelExtractor, DeepFeatureExtractor, RAGGraphBuilder
-from histocartography.visualization import InstanceImageVisualization
-from histocartography.utils.io import load_image
+from histocartography.utils.io import download_example_data
+from histocartography.preprocessing import (
+    ColorMergedSuperpixelExtractor,
+    DeepFeatureExtractor,
+    RAGGraphBuilder
+)
+from histocartography.visualization import OverlayGraphVisualization
 
 
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-i',
-        '--image_path',
-        type=str,
-        help='path to the images.',
-        required=True
+def generate_tissue_graph(image_path):
+    """
+    Generate a tissue graph for all the images in image path dir. 
+    """
+
+    # 1. get image path
+    image_fnames = glob(os.path.join(image_path, '*.png'))
+
+    # 2. define superpixel extractor 
+    superpixel_detector = ColorMergedSuperpixelExtractor(
+        nr_superpixels=50,
+        compactness=20,
+        blur_kernel_size=1,
+        threshold=0.02,
     )
-    parser.add_argument(
-        '-f',
-        '--fnames_path',
-        type=str,
-        help='path to file with list of images to process.',
-        required=True
+
+    # 3. define feature extractor 
+    feature_extractor = DeepFeatureExtractor(
+        architecture='resnet34',
+        patch_size=144,
+        resize_size=224
     )
-    parser.add_argument(
-        '-o',
-        '--out_path',
-        type=str,
-        help='path to save the output.',
-        required=True
-    )
-    return parser.parse_args()
-
-
-
-class TissueGraphBuilder:
-    """TissueGraphBuilder."""
-
-    def __init__(self, out_path, verbose=True, viz=True):
-        self.out_path = out_path
-        self.verbose = verbose
-        self.viz = viz
-        os.makedirs(os.path.join(out_path, 'tissue_graphs'), exist_ok=True)
-
-        self.superpixel_detector = ColorMergedSuperpixelExtractor(
-            nr_superpixels=50,
-            compactness=20,
-            blur_kernel_size=1,
-            threshold=0.01,
-        )
-        self.feature_extractor = DeepFeatureExtractor(architecture='resnet34', patch_size=144)
-        self.tissue_graph_builder = RAGGraphBuilder(add_loc_feats=True)
-
-        if self.viz:
-            self.visualiser = InstanceImageVisualization()
-            os.makedirs(os.path.join(out_path, 'visualization'), exist_ok=True)
-
-    def process(self, image_path, fnames_path):
-        """
-        Process the images listed in the fnames_path file. 
-        """
-
-        with open(fnames_path, 'r') as f:
-            image_fnames = [line.strip() for line in f]
-
-        for image_name in tqdm(image_fnames):
-            print('*** Testing image {}'.format(image_name))
-
-            # 1. load image
-            image = np.array(load_image(os.path.join(image_path, image_name)))
     
-            # 2. super pixel detection 
-            merged_superpixels, superpixels = self.superpixel_detector.process(image)
+    # 4. define graph builder 
+    tissue_graph_builder = RAGGraphBuilder(add_loc_feats=True)
 
-            # 3. super pixel feature extraction 
-            features = self.feature_extractor.process(image, merged_superpixels)
+    # 5. define graph visualizer
+    visualizer = OverlayGraphVisualization()
 
-            # 5. build the tissue graph
-            tissue_graph = self.tissue_graph_builder.process(
-                structure=merged_superpixels,
-                features=features,
-                image_size=(image.shape[1], image.shape[0])
-            )
+    # 6. process all the images 
+    for image_path in tqdm(image_fnames):
 
-            # 6. print graph properties
-            if self.verbose:
-                print('Number of nodes:', tissue_graph.number_of_nodes())
-                print('Number of edges:', tissue_graph.number_of_edges())
-                print('Node features:', tissue_graph.ndata['feat'].shape)
-                print('Node centroids:', tissue_graph.ndata['centroid'].shape)
-                print('Image size:', tissue_graph.gdata['image_size'])
+        # a. load image
+        _, image_name = os.path.split(image_path)
+        image = np.array(Image.open(image_path))
 
-            # 7. save DGL graph
-            tg_fname = image_name.replace('.png', '.bin')
-            save_graphs(os.path.join(self.out_path, 'tissue_graphs', tg_fname), [tissue_graph])
+        # b. extract superpixels 
+        superpixels, _ = superpixel_detector.process(image)
 
-            # 9. visualize the graph 
-            if self.viz:
-                out = self.visualiser.process(image, instance_map=merged_superpixels)
-                tg_fname = image_name.replace('.png', '_tg.png')
-                out.save(os.path.join(self.out_path, 'visualization', tg_fname))
+        # c. extract deep features 
+        features = feature_extractor.process(image, superpixels)
 
+        # d. build a Region Adjacency Graph (RAG)
+        graph = tissue_graph_builder.process(superpixels, features)
 
+        # e. save the graph  
+        fname = image_name.replace('.png', '.bin')
+        save_graphs(os.path.join('output', 'tissue_graphs', fname), [graph])
+        
+        # f. visualize and save the graph  
+        canvas = visualizer.process(image, graph, instance_map=superpixels)
+        canvas.save(os.path.join('output', 'tissue_graphs_viz', image_name))
+        
 if __name__ == "__main__":
-    args = parse_arguments()
-    graph_builder = TissueGraphBuilder(args.out_path)
-    graph_builder.process(args.image_path, args.fnames_path)
 
+    # 1. download dummy images 
+    download_example_data('output')
+
+    # 2. create output directories 
+    os.makedirs(os.path.join('output', 'tissue_graphs'), exist_ok=True)
+    os.makedirs(os.path.join('output', 'tissue_graphs_viz'), exist_ok=True)
+
+    # 3. generate tissue graphs  
+    generate_tissue_graph(image_path=os.path.join('output', 'images'))

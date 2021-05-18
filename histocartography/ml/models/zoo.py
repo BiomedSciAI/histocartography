971064ce6548b93e5766838130b4108d73fc6343
guillaumejaume <guillaume.jaume@epfl.ch>
2021-05-18 13:58:37 +0200

    fix: adapt setting in model zoo

--- histocartography/ml/models/zoo.py
+++ histocartography/ml/models/zoo.py
@@ -1,21 +1,21 @@
 # The models follow the naming convention:
 # <dataset_name>_<model_type>_<number_of_classes>_classes_<gnn_type>.pt
-# e.g., in bracs_tggnn_5_classes_pna:
+# e.g., in bracs_tggnn_7_classes_pna:
 # -btrained on bracs dataset
 # - with tggnn (Tissue Graph GNN) model
-# - and 5 classes
+# - and 7 classes
 # - with PNA GNN layers
 
 MODEL_NAME_TO_URL = {
     # CG-GNN
     'bracs_cggnn_3_classes_gin.pt': 'https://ibm.box.com/shared/static/pozkx0ngqjxdr34v5tpmckthts8m12u3.pt',
     'bracs_cggnn_5_classes_gin.pt': 'https://ibm.box.com/shared/static/uy2xeovpo001mb1edkwms5ccz1sqhdpz.pt',
-    'bracs_cggnn_7_classes_pna.pt': 'https://ibm.box.com/shared/static/i4xixoglstzkif53rc2cm4b568ei5wnf.pt',  # @TODO: update url
+    'bracs_cggnn_7_classes_pna.pt': 'https://ibm.box.com/shared/static/i4xixoglstzkif53rc2cm4b568ei5wnf.pt', 
     # TG-GNN
     'bracs_tggnn_3_classes_gin.pt': 'https://ibm.box.com/shared/static/aoogy0516lsp9vaxgw1tr9mdu5nycvvb.pt',
-    'bracs_tggnn_7_classes_pna.pt': 'https://ibm.box.com/shared/static/19q7kk2humvc6a8qedzg8rs5bny6qvrf.pt',  # @TODO: update url
+    'bracs_tggnn_7_classes_pna.pt': 'https://ibm.box.com/shared/static/19q7kk2humvc6a8qedzg8rs5bny6qvrf.pt', 
     # HACT
-    'bracs_hact_7_classes_pna.pt': 'https://ibm.box.com/shared/static/5v44c33cipdy7c2dhajkrfaywyrh2a5o.pt',   # @TODO: update url
+    'bracs_hact_7_classes_pna.pt': 'https://ibm.box.com/shared/static/5v44c33cipdy7c2dhajkrfaywyrh2a5o.pt', 
 }
 
 MODEL_NAME_TO_CONFIG = {
@@ -137,14 +137,14 @@ MODEL_NAME_TO_CONFIG = {
         'cg_gnn_params': {
             'layer_type': "pna_layer",
             'output_dim': 64,
-            'num_layers': 4,
+            'num_layers': 3,
             'readout_op': "lstm",
             'readout_type': "mean",
             'aggregators': "mean max min std",
             'scalers': "identity amplification attenuation",
             'avg_d': 4,
             'dropout': 0.,
-            'graph_norm': True,
+            'graph_norm': False,
             'batch_norm': True,
             'towers': 1,
             'pretrans_layers': 1,
@@ -156,14 +156,14 @@ MODEL_NAME_TO_CONFIG = {
         'tg_gnn_params': {
             'layer_type': "pna_layer",
             'output_dim': 64,
-            'num_layers': 4,
+            'num_layers': 3,
             'readout_op': "lstm",
             'readout_type': "mean",
             'aggregators': "mean max min std",
             'scalers': "identity amplification attenuation",
             'avg_d': 4,
             'dropout': 0.,
-            'graph_norm': True,
+            'graph_norm': False,
             'batch_norm': True,
             'towers': 1,
             'pretrans_layers': 1,
